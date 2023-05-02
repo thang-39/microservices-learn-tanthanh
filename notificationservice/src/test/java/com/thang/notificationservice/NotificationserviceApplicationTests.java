9ab09ec991577dda843ced0a59761429aaf47865
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-05-02 20:56:01 +0700

    init for notification service ( kafka)

---
+++ notificationservice/src/test/java/com/thang/notificationservice/NotificationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thang.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
